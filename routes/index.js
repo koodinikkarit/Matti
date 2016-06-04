f715a5d17d1fd2a318d5139d6a97ce4e06157f49
Jaska <aa>
2016-06-04 12:41:32 +0300

    Matti.js osaa kysyä kaikkien yhteyksien tilat ja asettaa video yhteyksiä.

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.end("<h1>(url)/set?con=(con)&cpu=(cpu)&ip=(ip)&port=(port) </h1>");
-});
-
-module.exports = router;
--- sql/profile_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE profiles (
-	id INT NOT NULL AUTO_INCREMENT,
-	name INT NOT NULL,
-	token INT NOT NULL,
-	PRIMARY KEY(id),
-	FOREIGN KEY(name) REFERENCES names(id),
-	FOREIGN KEY(token) REFERENCES token(id)
-);
