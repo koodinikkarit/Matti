2c6118bde995fa639690b0a5ec5515e8f4a18132
Jaska <aa>
2016-08-17 22:43:30 +0300

    Uusia metodeja profile moduuliin.

--- profile.js
+++ profile.js
@@ -1,5 +1,15 @@
+var MatrixProfile = require("./MatrixProfile");
+
 module.exports = function(id, manager) {
 	var that = {};
+
+	that.getId = function() {
+		return id;
+	}
+
+	that.getToken = function (callback) {
+
+	}
 
 	that.getMatrixs = function (callback) {
 		var matrixProfiles = [];
