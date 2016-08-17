64a17e57285a9f76e1dc15d0c89baf6a893f5eb9
Jaska <aa>
2016-08-17 22:35:49 +0300

    Lisätty toiminnallisuus hakea profiilin matriisit.

--- profile.js
+++ profile.js
@@ -1,6 +1,16 @@
 module.exports = function(id, manager) {
 	var that = {};
 
+	that.getMatrixs = function (callback) {
+		var matrixProfiles = [];
+		manager.getMatrixsForProfile(that, function (matrixs) {
+			matrixs.forEach(function(matrix) {
+				console.log(matrix);
+				matrixProfiles.push(new MatrixProfile(that, matrix, manager));
+			});	
+			if (callback) callback(matrixProfiles);		
+		});		
+	}
 
 	
 	return that;
