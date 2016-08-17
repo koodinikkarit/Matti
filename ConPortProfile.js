9daa9738213ecda2e15c12c93fe9cd8b46daf617
Jaska <aa>
2016-08-17 22:35:09 +0300

    Luotu ConPortProfile moduuli.

---
+++ ConPortProfile.js
@@ -0,0 +1,17 @@
+module.exports = function(conPort, profile, manager) {
+	var that = {};
+
+	that.setName = function(name) {
+
+	}
+
+	that.canTurnOff = function () {
+
+	}
+
+	that.canTurnOn = function () {
+		
+	}
+
+	return that;
+}
